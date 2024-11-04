62c1347f416bb3d098fc3320f79de0942bd04a36
Hissen <100237118+FSuhas@users.noreply.github.com>
2024-11-04 17:05:29 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -125,4 +125,4 @@ flask_thread = threading.Thread(target=run_flask)
 flask_thread.start()
 
 # Remplacez 'YOUR_TOKEN' par le token de votre bot
-bot.run(TOKEN)  # Remplacez par votre token
+bot.run(DISCORD_TOKEN)  # Remplacez par votre token
